eafa6cb9aa4c20c1d685b6f89d8fa5ed4704466d
Austinb96 <austin.m.babcock@gmaIl.com>
2017-05-06 19:31:20 -0500

    more pages

--- Webpack/_site/assets/js/build.js
+++ Webpack/_site/assets/js/build.js
@@ -63,103 +63,100 @@
 /******/ 	__webpack_require__.p = "";
 
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ 	return __webpack_require__(__webpack_require__.s = 12);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: Error\n    at C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\webpack\\lib\\NormalModule.js:141:35\n    at C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\loader-runner\\lib\\LoaderRunner.js:364:11\n    at C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\loader-runner\\lib\\LoaderRunner.js:230:18\n    at context.callback (C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at Object.onRender [as callback] (C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\sass-loader\\index.js:273:13)\n    at Object.<anonymous> (C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\async\\dist\\async.js:2234:31)\n    at apply (C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\async\\dist\\async.js:20:25)\n    at Object.<anonymous> (C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\async\\dist\\async.js:56:12)\n    at Object.callback (C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\async\\dist\\async.js:840:16)\n    at options.error (C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\node-sass\\lib\\index.js:294:32)");
 
 /***/ }),
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__.p + "_site/contact.html";
+module.exports = __webpack_require__.p + "_site/alyssaride.html";
 
 /***/ }),
 /* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__.p + "_site/donate.html";
+module.exports = __webpack_require__.p + "_site/contact.html";
 
 /***/ }),
 /* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__.p + "_site/index.html";
+module.exports = __webpack_require__.p + "_site/donate.html";
 
 /***/ }),
 /* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "_site/index.html";
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "_site/media.html";
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "_site/news.html";
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "_site/ourgiving.html";
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "_site/ourmission.html";
+
+/***/ }),
+/* 9 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "_site/scholarship.html";
+
+/***/ }),
+/* 10 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "_site/shirt.html";
+
+/***/ }),
+/* 11 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "_site/sponsors.html";
+
+/***/ }),
+/* 12 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
 __webpack_require__(0);
+__webpack_require__(4);
 __webpack_require__(3);
 __webpack_require__(2);
 __webpack_require__(1);
+__webpack_require__(5);
+__webpack_require__(6);
+__webpack_require__(7);
 __webpack_require__(8);
 __webpack_require__(9);
 __webpack_require__(10);
 __webpack_require__(11);
-__webpack_require__(12);
-__webpack_require__(15);
-__webpack_require__(13);
-__webpack_require__(14);
-
-/***/ }),
-/* 5 */,
-/* 6 */,
-/* 7 */,
-/* 8 */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__.p + "_site/alyssaride.html";
-
-/***/ }),
-/* 9 */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__.p + "_site/media.html";
-
-/***/ }),
-/* 10 */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__.p + "_site/news.html";
-
-/***/ }),
-/* 11 */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__.p + "_site/ourgiving.html";
-
-/***/ }),
-/* 12 */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__.p + "_site/ourmission.html";
-
-/***/ }),
-/* 13 */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__.p + "_site/shirt.html";
-
-/***/ }),
-/* 14 */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__.p + "_site/sponsors.html";
-
-/***/ }),
-/* 15 */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__.p + "_site/scholarship.html";
 
 /***/ })
 /******/ ]);
