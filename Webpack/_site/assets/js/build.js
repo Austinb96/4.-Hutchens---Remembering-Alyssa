1b1cd63e15d8fa8dae96fbc4d215e8f36dfb0d5e
Austinb96 <austin.m.babcock@gmaIl.com>
2017-05-06 21:40:37 -0500

    more pages complete

--- Webpack/_site/assets/js/build.js
+++ Webpack/_site/assets/js/build.js
@@ -70,7 +70,7 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: Error\n    at C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\webpack\\lib\\NormalModule.js:141:35\n    at C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\loader-runner\\lib\\LoaderRunner.js:364:11\n    at C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\loader-runner\\lib\\LoaderRunner.js:230:18\n    at context.callback (C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at Object.onRender [as callback] (C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\sass-loader\\index.js:273:13)\n    at Object.<anonymous> (C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\async\\dist\\async.js:2234:31)\n    at apply (C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\async\\dist\\async.js:20:25)\n    at Object.<anonymous> (C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\async\\dist\\async.js:56:12)\n    at Object.callback (C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\async\\dist\\async.js:840:16)\n    at options.error (C:\\Users\\austi\\Desktop\\4. Hutchens - Remembering Alyssa\\Webpack\\node_modules\\node-sass\\lib\\index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 /* 1 */
