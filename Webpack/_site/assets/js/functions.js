14698e883312d4759b01fe3f14a3c1d167ef9182
Austinb96 <austin.m.babcock@gmaIl.com>
2017-05-15 05:28:12 -0500

    finished ourgiving
    yaaaay

--- Webpack/_site/assets/js/functions.js
+++ Webpack/_site/assets/js/functions.js
@@ -1,28 +1,44 @@
+var clicked = false;
+
 $(document).ready(function(){
     $(".hamburger").click(function(){
         $(".hamburger").toggleClass("open");
         $(".main").toggleClass("mainopen");
     });
-    $(".giving1 img").click(function(){
+    $(".giving1").click(function(){
         $(".selection1").toggleClass('hide');
         $(".selection2, .selection3").addClass('hide');
-        if (".selection2").hasClass('hide') || (".selection3").hasClass('hide')  {
-        }
-        else {
-          $(".selectioncontent").toggleClass('hide');
-        };
         $("#triangle-up1").toggleClass('hidet');
         $("#triangle-up2, #triangle-up3").addClass('hidet');
-    });
-    $(".giving2 img").click(function(){
+        if (!clicked){
+              clicked = true;
+              setTimeout(function(){ clicked = false; }, 1000);
+              }else{
+                return false;
+            };
+        });
+    $(".giving2").click(function(){
         $(".selection2").toggleClass('hide');
         $(".selection1, .selection3").addClass('hide');
-        if (".selection1").hasClass('hide') || (".selection3").hasClass('hide')  {
-        }
-        else {
-          $(".selectioncontent").toggleClass('hide');
-        };
         $("#triangle-up2").toggleClass('hidet');
         $("#triangle-up1, #triangle-up3").addClass('hidet');
+        if (!clicked){
+              clicked = true;
+              setTimeout(function(){ clicked = false; }, 1000);
+              }else{
+                return false;
+              };
+    });
+    $(".giving3").click(function(){
+        $(".selection3").toggleClass('hide');
+        $(".selection1, .selection2").addClass('hide');
+        $("#triangle-up3").toggleClass('hidet');
+        $("#triangle-up1, #triangle-up2").addClass('hidet');
+        if (!clicked){
+              clicked = true;
+              setTimeout(function(){ clicked = false; }, 1000);
+              }else{
+                return false;
+              };
     });
 });
